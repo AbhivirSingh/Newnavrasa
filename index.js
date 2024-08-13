d6e2326f57ed3a1b418f3dec4d01e98a51584830
Nitesh saini <itsniteshsaini6376@gmail.com>
2024-08-13 17:35:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const i = document.querySelector('body > img:first-child');
 
     setTimeout(() => {
-        i.style.height = '24.9vw';
-        i.style.width = '20vw';
+        i.style.height = '40vw';
+        i.style.width = '30vw';
         i.style.right = '10vw';
-        i.style.top = '26vh';
+        i.style.top = '8vh';
         i.style.borderRadius = '1rem';
     }, 1000);
 
@@ -29,10 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
             i.style.right = '5vw';
             i.style.top = '22vh';
         } else {
-            i.style.height = '24.9vw';
-            i.style.width = '20vw';
+            i.style.height = '40vw';
+            i.style.width = '30vw';
             i.style.right = '10vw';
-            i.style.top = '26vh';
+            i.style.top = '8vh';
             i.style.borderRadius = '1rem';
         }
     };
@@ -49,11 +49,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 AOS.init();
 
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
 
-    menuToggle.addEventListener('click', function () {
-        menu.classList.toggle('hidden');
-    });
+
+// vedio scrolling effect
+
+window.addEventListener('scroll', function () {
+    const video = document.getElementById('video');
+    const videoContainer = document.querySelector('.video-container');
+    const containerTop = videoContainer.offsetTop;
+    const scrollPosition = window.scrollY + window.innerHeight;
+
+    if (scrollPosition > containerTop) {
+        const percentage = Math.min((scrollPosition - containerTop) / window.innerHeight, 1);
+        video.style.width = `${percentage * 100}vw`;
+        video.style.height = `${percentage * 100}vh`;
+    }
 });
